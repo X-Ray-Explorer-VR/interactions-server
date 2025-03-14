9c24268dcef4c7c4a8b3e61e1c55e952f126fb89
Alejandro Suárez <alejandro-hdez115@outlook.com>
2025-03-13 18:45:03 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws');
+require('colors');
 
-const wss = new WebSocketServer({ port: 2567 });
+const wss = new WebSocketServer({ port: 8080 });
+
+// Save required sockets
+let frontSocket, oculusSocket;
 
 wss.on('connection', function connection(ws) {
-  console.log('Client connected!');
+  console.log('Client connected'.bgGreen.black);
 
   ws.on('error', console.error);
 
-  ws.on('close', function close() {
-    console.log('Client disconnected!');
+  ws.on('close', () => {
+    console.log('Client disconnected'.bgRed);
+
+    if (frontSocket === ws) {
+      frontSocket = null;
+
+      console.log('Front-end client disconnected'.bgYellow.black);
+    } else if (oculusSocket === ws) {
+      oculusSocket === null;
+
+      if (frontSocket) {
+        frontSocket.send('Oculus client: Disconnected');
+      }
+
+      console.log('Oculus client disconnected'.bgYellow.black);
+    }
   });
 
-  ws.on('message', function message(data, isBinary) {
-    console.log('Received: %s', data);
+  ws.on('message', (data) => {
+    console.log(`Message from client: ${data}`);
+
+    try {
+      // Process message
+      const dividedMessage = data.toString().split(': ');
+      const clientName = dividedMessage[0];
+      const clientData = dividedMessage[1];
+
+      // Check client
+      if (clientName.includes('Oculus')) {
+        // Check action
+        if (clientData.includes('Connected')) {
+          oculusSocket = ws;
+
+          console.log('Oculus client connected'.bgBlue);
+        }
+        // Send data to front-end
+        sendToFrontend(data);
+      } else if (clientName.includes('Front-end')) {
+        // Check action
+        if (clientData.includes('Connected')) {
+          frontSocket = ws;
+
+          console.log('Front-end client connected'.bgBlue);
+        }
+      }
+    } catch (ex) {
+      console.error(ex);
+    }
   });
 });
+
+/**
+ * Sends a message to the front-end socket client
+ * @param {string} data - Message to send
+ */
+function sendToFrontend(data) {
+  if (frontSocket && frontSocket.readyState === WebSocket.OPEN) {
+    frontSocket.send(data);
+  }
+}
